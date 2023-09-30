7383f5d2fe9498d02d19b7901071cb70af54f0b3
Febriansyah <fbriansyah@gmail.com>
2023-09-30 19:10:58 +0700

    add paseto configuration

--- util/config.go
+++ util/config.go
@@ -1,15 +1,20 @@
 package util
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type Config struct {
-	Environment  string `mapstructure:"ENVIRONMENT"`
-	RedisAddress string `mapstructure:"REDIS_ADDRESS"`
-	GrpcPort     int    `mapstructure:"GRPC_PORT"`
+	Environment          string        `mapstructure:"ENVIRONMENT"`
+	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
+	GrpcPort             int           `mapstructure:"GRPC_PORT"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 }
 
 // LoadConfig reads configuration from file or environment variables.

cd1ce918707edeb45b110c6ba0548657043dba33
Febriansyah <fbriansyah@gmail.com>
2023-09-30 19:09:03 +0700

    remove un-used config

--- util/config.go
+++ util/config.go
@@ -7,13 +7,9 @@ import (
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type Config struct {
-	Environment          string `mapstructure:"ENVIRONMENT"`
-	DBDriver             string `mapstructure:"DB_DRIVER"`
-	DBSource             string `mapstructure:"DB_SOURCE"`
-	MigrationURL         string `mapstructure:"MIGRATION_URL"`
-	HTTPServerAddress    string `mapstructure:"HTTP_SERVER_ADDRESS"`
-	GrpcPort             int    `mapstructure:"GRPC_PORT"`
-	SessionServerAddress string `mapstructure:"SESSION_SERVER_ADDRESS"`
+	Environment  string `mapstructure:"ENVIRONMENT"`
+	RedisAddress string `mapstructure:"REDIS_ADDRESS"`
+	GrpcPort     int    `mapstructure:"GRPC_PORT"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
